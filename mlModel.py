bd90f4ae1f71e3d60038e63cb4e29716f002f9ca
Anuj Dubey <105970366+dmanuj4@users.noreply.github.com>
2023-11-05 15:54:43 +0530

    Update mlModel.py

--- mlModel.py
+++ mlModel.py
@@ -1,24 +1,18 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
-# plt.style.use('./deeplearning.mplstyle')
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
 RANDOM_STATE = 55
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
 
 def normalize(x):
     mu = np.mean(x)
@@ -58,51 +52,13 @@ X_train_norm = normalize(np.array(X_train))
 X_val_norm = normalize(np.array(X_val))
 
 
-min_samples_split_list = [2,10, 30, 50, 100, 200, 300, 700] ## If the number is an integer, then it is the actual quantity of samples,
-max_depth_list = [1,2, 3, 4, 8, 16, 32, 64, None] # None means that there is no depth limit.
+min_samples_split_list = [2,10, 30, 50, 100, 200, 300, 700] 
+max_depth_list = [1,2, 3, 4, 8, 16, 32, 64, None] 
 
 #Now lets try Randomforest
 n_estimators_list = [10,50,100,500]
 
-accuracy_list_train = []
-accuracy_list_val = []
-for min_samples_split in min_samples_split_list:
-    # You can fit the model at the same time you define it, because the fit function returns the fitted estimator.
-    model = RandomForestClassifier(min_samples_split = min_samples_split,
-                                   random_state = RANDOM_STATE).fit(X_train,y_train)
-    predictions_train = model.predict(X_train) ## The predicted values for the train dataset
-    predictions_val = model.predict(X_val) ## The predicted values for the test dataset
-    accuracy_train = accuracy_score(predictions_train,y_train)
-    accuracy_val = accuracy_score(predictions_val,y_val)
-    accuracy_list_train.append(accuracy_train)
-    accuracy_list_val.append(accuracy_val)
 
-
-accuracy_list_train = []
-accuracy_list_val = []
-for max_depth in max_depth_list:
-    # You can fit the model at the same time you define it, because the fit function returns the fitted estimator.
-    model = RandomForestClassifier(max_depth = max_depth,
-                                   random_state = RANDOM_STATE).fit(X_train,y_train)
-    predictions_train = model.predict(X_train) ## The predicted values for the train dataset
-    predictions_val = model.predict(X_val) ## The predicted values for the test dataset
-    accuracy_train = accuracy_score(predictions_train,y_train)
-    accuracy_val = accuracy_score(predictions_val,y_val)
-    accuracy_list_train.append(accuracy_train)
-    accuracy_list_val.append(accuracy_val)
-
-accuracy_list_train = []
-accuracy_list_val = []
-for n_estimators in n_estimators_list:
-    # You can fit the model at the same time you define it, because the fit function returns the fitted estimator.
-    model = RandomForestClassifier(n_estimators = n_estimators,
-                                   random_state = RANDOM_STATE).fit(X_train,y_train)
-    predictions_train = model.predict(X_train) ## The predicted values for the train dataset
-    predictions_val = model.predict(X_val) ## The predicted values for the test dataset
-    accuracy_train = accuracy_score(predictions_train,y_train)
-    accuracy_val = accuracy_score(predictions_val,y_val)
-    accuracy_list_train.append(accuracy_train)
-    accuracy_list_val.append(accuracy_val)
 
 
 """max_depht = 4
